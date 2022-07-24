d78f4b1fd7f027acafb98d1f4d72ffaa8ad1157e
Michael <michael.c.montgomery@gmail.com>
2022-07-24 12:50:21 -0500

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        sorted_characters = @word.chars.sort
+        matched_words = array.select {|word| word.chars.sort == sorted_characters}
+        matched_words  
+    end
+end
